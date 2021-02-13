991caf694736d8a9fe0ee6e987a02efdaa3fcbcc
Berat-Dzhevdetov <56674380+Berat-Dzhevdetov@users.noreply.github.com>
2021-02-13 11:32:18 +0200

    Update 8.Single-Files.sql

--- 8.Single-Files.sql
+++ 8.Single-Files.sql
@@ -1,4 +1,4 @@
-SELECT f1.Id,f1.[Name],CONCAT(f1.Size,'KB') AS Size FROM Files as f1
+SELECT f1.Id,f1.[Name],CONCAT(f1.Size,'KB') AS Size FROM Files AS f1
 LEFT JOIN Files AS f2 ON f2.ParentId = f1.Id
 WHERE f2.Id IS NULL
 ORDER BY f1.Id,f1.[Name],f1.Size DESC
