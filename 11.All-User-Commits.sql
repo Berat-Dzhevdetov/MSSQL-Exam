832a5546a58d050e3344eeaf706f00621d4bb02d
Berat-Dzhevdetov <56674380+Berat-Dzhevdetov@users.noreply.github.com>
2021-02-13 11:29:21 +0200

    Update 11.All-User-Commits.sql

--- 11.All-User-Commits.sql
+++ 11.All-User-Commits.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION udf_AllUserCommits(@username VARCHAR(MAX))
 RETURNS INT
 AS BEGIN
 	RETURN (
-		SELECT Count(*) as [Count] FROM Commits AS c
+		SELECT Count(*) AS [Count] FROM Commits AS c
 		JOIN Users AS u ON c.ContributorId = u.Id
 		WHERE u.Username = @username
 		)
