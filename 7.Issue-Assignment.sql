4e8aa52154c16986ab27ca7e31be2a0b7bf7406e
Berat-Dzhevdetov <56674380+Berat-Dzhevdetov@users.noreply.github.com>
2021-02-13 11:31:53 +0200

    Update 7.Issue-Assignment.sql

--- 7.Issue-Assignment.sql
+++ 7.Issue-Assignment.sql
@@ -1,5 +1,5 @@
 SELECT i.Id, CONCAT(u.Username, ' : ', i.Title) AS IssueAssignee 
-FROM Issues as i
-JOIN Repositories AS r on r.Id=i.RepositoryId
+FROM Issues AS i
+JOIN Repositories AS r ON r.Id=i.RepositoryId
 JOIN Users AS u ON I.AssigneeId = u.Id
 ORDER BY i.Id DESC,i.AssigneeId
